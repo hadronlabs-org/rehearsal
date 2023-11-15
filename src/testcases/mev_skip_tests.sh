97b778f04fcecc0ac85b5bb1df30d8a0597b6b37
nhpd <dmitry.klpv@gmail.com>
2023-11-15 18:20:27 +0400

    fix variables for mev test

--- src/testcases/mev_skip_tests.sh
+++ src/testcases/mev_skip_tests.sh
@@ -1,13 +1,20 @@
 DESTINATION=neutron12ds5vg0my4chtexqlwjw4eflt55qp2dxgsywae
 
-# ==================== mainnet fork (PICK)
-GAS_PRICES="0.5untrn"
+# ================= CONFIG =========
+GAS_PRICES="1untrn"
 CHAINID="neutron-1"
-TEST_WALLET="TODO"
-KEYS_HOME="~/.neutrond-mainnet" # TODO
-NODE="https://rpc-talzor.neutron-1.neutron.org:443" # TODO
+TEST_WALLET="neutrond_mainnet_rehearsal" # neutron1kyn3jx88wvnm3mhnwpuue29alhsatwzrpkwhu6
+KEYS_HOME="~/.neutrond-rehearsal"
+NODE="http://37.27.55.151:26657"
+RECIPIENT="neutron19wv4pq0qzpgthyzh43mlkr0tcz6gpeta7587zq"
 
-# POB
+# ==================================
+
+# ================= TEST ===========
 
 ENCODED=$(cat "/Users/nhpd/p2p/rehearsal/tx.binary")
 RES=$(neutrond tx auction auction-bid ${TEST_WALLET} "1000untrn" $ENCODED --gas-prices ${GAS_PRICES} --chain-id ${CHAINID} --keyring-backend test --home ${KEYS_HOME} --node ${NODE} --broadcast-mode=sync -y --output json --timeout-height 10000)
+
+neutrond q bank balances $RECIPIENT --node $NODE --output json
+
+# ==================================
