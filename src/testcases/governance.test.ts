141a8a3278a1e4dea92b5e55f530fa865107ea93
sotnikov-s <ssotnikov93@gmail.com>
2023-11-15 15:33:55 +0300

    split params change test to params change via bindings and other Neutron gov tests

--- src/testcases/governance.test.ts
+++ src/testcases/governance.test.ts
@@ -13,7 +13,7 @@ export const ADMIN_MODULE_ADDRESS = 'neutron1hxskfdxpp5hqgtjj6am6nkjefhfzj359x0a
 const WALLET_MNEMONIC =
     'banner spread envelope side kite person disagree path silver will brother under couch edit food venture squirrel civil budget number acquire point work mass';
 
-describe('Parameters change via Neutron DAO', () => {
+describe('Governance', () => {
     const context: {
         wallet?: DirectSecp256k1HdWallet;
         client?: SigningCosmWasmClient;
@@ -54,88 +54,6 @@ describe('Parameters change via Neutron DAO', () => {
             );
         })
     });
-
-    // This test covers parameters change via update params bindings
-    // describe('change icq parameters via proposal', () => {
-    //     let paramsBefore: ParamsInterchainqueriesInfo;
-    //     test('get icq module params', async () => {
-    //     const resp = await axios.get(
-    //         `http://127.0.0.1:1317/neutron/interchainqueries/params`,
-    //         );
-    //         paramsBefore = resp.data.params;
-    //     })
-        
-    //     let proposalId: number;
-    //     test('create a proposal', async () => {
-    //         let newParams = { ...paramsBefore};
-    //         newParams.query_submit_timeout += 1;
-    //         const propMsg = updateInterchainqueriesParamsProposal(newParams);
-
-    //         const proposalTx = await context.client.execute(
-    //             walletAddr,
-    //             PRE_PROPOSE_CONTRACT,
-    //             {
-    //                 propose: {
-    //                     msg: {
-    //                         propose: {
-    //                             title: 'increase ICQ module\'s query_submit_timeout',
-    //                             description: 'increase ICQ module\'s query_submit_timeout in testing purposes',
-    //                             msgs: [propMsg],
-    //                         },
-    //                     },
-    //                 },
-    //             },
-    //             'auto',
-    //             undefined,
-    //             [{ amount: '1000', denom: UNTRN_DENOM }],
-    //         );
-
-    //         const attribute = getEventAttribute(
-    //             (proposalTx as any).events,
-    //             'wasm',
-    //             'proposal_id',
-    //         );
-
-    //         proposalId = parseInt(attribute);
-    //         expect(proposalId).toBeGreaterThanOrEqual(0);
-    //     });
-
-    //     test('vote for proposal', async () => {
-    //         await context.client.execute(
-    //             walletAddr,
-    //             PROPOSAL_CONTRACT,
-    //             { vote: { proposal_id: proposalId, vote: 'yes' } },
-    //             'auto',
-    //             undefined,
-    //         );
-    //     });
-
-    //     test('expect proposal passed', async () => {
-    //         const proposal: SingleChoiceProposal = await context.client.queryContractSmart(
-    //             PROPOSAL_CONTRACT,
-    //             {proposal: {proposal_id: proposalId}},
-    //         );
-    //         expect(proposal.proposal.status).toEqual('passed');
-    //     });
-
-    //     test('execute proposal', async () => {
-    //         await context.client.execute(
-    //             walletAddr,
-    //             PROPOSAL_CONTRACT,
-    //             { execute: { proposal_id: proposalId } },
-    //             'auto',
-    //             undefined,
-    //         );
-    //     });
-
-    //     test('compare new parameters to the previous ones', async () => {
-    //         const resp = await axios.get(
-    //             `http://127.0.0.1:1317/neutron/interchainqueries/params`,
-    //         );
-    //         const paramsAfter: ParamsInterchainqueriesInfo = resp.data.params;
-    //         expect(paramsAfter.query_submit_timeout).toEqual(paramsBefore.query_submit_timeout + 1);
-    //     });
-    // });
 
     describe('change cron parameters via stargate proposal', () => {
         let paramsBefore: ParamsCronInfo;
@@ -316,34 +234,6 @@ type ParamsCronInfo = {
     security_address: string;
     limit: number;
 };
-
-type ParamsInterchainqueriesInfo = {
-    query_submit_timeout: number;
-    query_deposit: V1Beta1DecCoin;
-    tx_query_removal_limit: number;
-};
-
-const updateInterchainqueriesParamsProposal = (
-    info: ParamsInterchainqueriesInfo,
-): any => ({
-    custom: {
-        submit_admin_proposal: {
-            admin_proposal: {
-                proposal_execute_message: {
-                    message: JSON.stringify({
-                        '@type': '/neutron.interchainqueries.MsgUpdateParams',
-                        authority: ADMIN_MODULE_ADDRESS,
-                        params: {
-                            query_submit_timeout: info.query_submit_timeout,
-                            query_deposit: null,
-                            tx_query_removal_limit: info.tx_query_removal_limit,
-                        },
-                    }),
-                },
-            },
-        },
-    },
-});
 
 const getEventAttribute = (
     events: { type: string; attributes: { key: string; value: string }[] }[],
