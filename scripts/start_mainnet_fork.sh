db933c724c83fb3b1dee383746c5d6716e2f3a1f
nhpd <dmitry.klpv@gmail.com>
2024-05-30 17:14:35 +0400

    fix

--- scripts/start_mainnet_fork.sh
+++ scripts/start_mainnet_fork.sh
@@ -5,8 +5,8 @@ SNAPSHOT_DOWNLOAD_URL="https://raw-snapshots.neutron.org"
 
 if [ "$FIRST_RUN" = "true" ]; then
     echo "Copying into data initial state"
-    rm -rf /opt/neutron/data
-    mv /opt/neutron/initial_data /opt/neutron/data
+    rm -rf /opt/neutron/data/*
+    cp -r /opt/neutron/initial_data/* /opt/neutron/data
     neutrond tendermint unsafe-reset-all --home /opt/neutron/data
 
     echo "Previous state backup not found, starting from genesis..."
