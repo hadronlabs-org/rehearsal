e99d7ae02746b88d7b800f2bc5ab2ce313fcec10
nhpd <dmitry.klpv@gmail.com>
2024-06-05 00:49:12 +0400

    fix ports

--- scripts/start_upgraded_neutron.sh
+++ scripts/start_upgraded_neutron.sh
@@ -5,5 +5,6 @@ crudini --set /opt/neutron/data/config/app.toml oracle metrics_enabled true
 crudini --set /opt/neutron/data/config/app.toml telemetry enabled true
 
 crudini --set /opt/neutron/data/config/config.toml instrumentation prometheus true
+# crudini --set /opt/neutron/data/config/config.toml instrumentation prometheus_listen_addr "\"tcp://0.0.0.0:26660\""
 
 neutrond_new start --home /opt/neutron/data --x-crisis-skip-assert-invariants --iavl-disable-fastnode false
