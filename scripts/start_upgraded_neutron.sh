444bf822e34a9389c142e61a3c63b7c2b0ffb827
nhpd <dmitry.klpv@gmail.com>
2024-06-04 19:13:10 +0400

    add oracle config before update starts

--- scripts/start_upgraded_neutron.sh
+++ scripts/start_upgraded_neutron.sh
@@ -1,1 +1,6 @@
+crudini --set /opt/neutron/data/config/app.toml oracle enabled true
+crudini --set /opt/neutron/data/config/app.toml oracle oracle_address "\"oracle:8080\""
+crudini --set /opt/neutron/data/config/app.toml oracle client_timeout "\"500ms\""
+crudini --set /opt/neutron/data/config/app.toml oracle metrics_enabled true
+
 neutrond_new start --home /opt/neutron/data --x-crisis-skip-assert-invariants --iavl-disable-fastnode false
