fa0c724711a25db21d27d55e71b1614950b8ddae
Vladislav Vasilev <81014877+faust403@users.noreply.github.com>
2025-02-07 13:59:30 +0100

    fix

--- custom/config.sh
+++ custom/config.sh
@@ -15,8 +15,68 @@ SINGLE_PROPOSAL_CONFIG='{
   "min_voting_period": null,
   "allow_revoting": false,
   "dao": "neutron1suhgf5svhu4usrurvxzlgn54ksxmn8gljarjtxqnapv8kjnp4nrstdxvff",
-  "close_proposal_on_execution_failure": true
+  "close_proposal_on_execution_failure": false
 }'
+
+function add_coin() {
+    local in="$1"
+    local out="$2"
+    local addr="$3"
+    local denom="$4"
+    local amount="$5"
+
+    local tmp_file=$(mktemp)
+
+    jq --arg addr "$addr" --arg denom "$denom" --arg amount "$amount" '
+        .app_state.bank.balances |= (
+            if any(.address == $addr) then
+                map(
+                    if .address == $addr then
+                        if any(.coins[]; .denom == $denom) then
+                            .coins |= map(
+                                if .denom == $denom then
+                                    .amount = (.amount | tonumber + ($amount | tonumber) | tostring)
+                                else
+                                    .
+                                end
+                            )
+                        else
+                            .coins += [{"denom": $denom, "amount": $amount}]
+                        end |
+                        .coins |= sort_by(.denom)
+                    else
+                        .
+                    end
+                )
+            else
+                . + [{"address": $addr, "coins": [{"denom": $denom, "amount": $amount}]}]
+            end
+        ) |
+        .app_state.bank.supply |= (
+            if any(.denom == $denom) then
+                map(
+                    if .denom == $denom then
+                        .amount = (.amount | tonumber + ($amount | tonumber) | tostring)
+                    else
+                        .
+                    end
+                )
+            else
+                . + [{"denom": $denom, "amount": $amount}]
+            end |
+            sort_by(.denom)
+        )
+    ' "$in" > "$tmp_file"
+
+    if [ $? -eq 0 ]; then
+        mv "$tmp_file" "$out"
+    else
+        echo "Error: jq command failed"
+        rm "$tmp_file"
+    fi
+}
+
+add_coin $INPUT_GENESIS_FILE $INPUT_GENESIS_FILE $MAIN_WALLET untrn 1000000000000
 
 echo "Applying single proposal contract custom config: $SINGLE_PROPOSAL_CONFIG"
 
